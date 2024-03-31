65547268a0fdda336581527456777ce82a32dedd
Harsh <hjain2083@gmail.com>
2024-03-31 15:41:07 +0530

    convert in node environment

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// middleware
+app.use(express.static(path.join(__dirname, "/public/bootstrap/css")));
+app.use(express.static(path.join(__dirname, "/public/bootstrap/js")));
+app.use(express.static(path.join(__dirname, "/public/assets")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.get("/login", (req, res) => {
+    res.send("this route will work");
+})
+
+app.listen("8080", (err, res) => {
+    console.log("Server is running on port 8080");
+})
