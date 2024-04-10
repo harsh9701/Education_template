bed890f4a7cdb5029e3c5a8ec3754e416e970d13
Harsh <hjain2083@gmail.com>
2024-04-10 20:20:45 +0530

    Implement modal

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
-})
+});
 
 app.get("/login", (req, res) => {
     res.send("this route will work");
-})
+});
 
 app.get("/signup", (req, res) => {
     res.render("signup.ejs");
-})
+});
 
 app.listen("8080", (err, res) => {
     console.log("Server is running on port 8080");
