af6483cc70239a5291c9aa287e7507faf112e797
Harsh <hjain2083@gmail.com>
2024-05-17 16:49:14 +0530

    Implement Authentication feature

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 const User = require("./models/user.js");
 const { userSchema } = require("./schema.js");
 
@@ -37,6 +42,7 @@ app.use(express.static(path.join(__dirname, "/public/assets")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -49,16 +55,51 @@ app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
 
+app.post("/login", async (req, res) => {
+    let { user } = req.body;
+    let isExistUser = await User.findOne({ username: user.name });
+
+    if(!isExistUser) {
+        return res.send(`User doesn't exist with this username - ${user.name}`);
+    }
+
+    bcrypt.compare(user.password, isExistUser.password, (err, result) => {
+        if(result) {
+            let token = jwt.sign({ email: isExistUser.email }, "shhhhhhhhh");
+            res.cookie("token", token);
+            res.send("Login");
+        } else {
+            res.send("Wrong Password");
+        }
+    })
+});
+
 app.get("/signup", (req, res) => {
     res.render("signup.ejs");
 });
 
 app.post("/signup", validateUser, async (req, res) => {
     let { user } = req.body;
-    const newUser = new User(user);
-    await newUser.save();
-    res.send("User Created");
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, async (err, hash) => {
+            user.password = hash;
+            const newUser = new User(user);
+            await newUser.save();
+
+            let token = jwt.sign({ email: newUser.email }, "shhhhhhhhh");
+            res.cookie("token", token);
+
+            res.redirect("/");
+        });
+    });
+
 });
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", "");
+    res.redirect("/");
+})
 
 app.listen("8080", (err, res) => {
     console.log("Server is running on port 8080");
